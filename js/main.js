d9c0bd7a7fb05ff4dbade1d7784234092f18b3d8
Moshik Alper <moshikalper@gmail.com>
2024-06-14 21:52:30 +0300

    Limit the ball size to 400px and return it to 100px if it gets to 400px

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@
 function onBallClick(elBall) {
     elBall.style.width = parseInt(elBall.clientWidth) + 50 + 'px'
     elBall.style.height = parseInt(elBall.clientHeight) + 50 + 'px'
+
+    if (elBall.style.width === '400px' && elBall.style.height === '400px') {
+        elBall.style.height = '100px'
+        elBall.style.width = '100px'
+    }
+    
 } 
